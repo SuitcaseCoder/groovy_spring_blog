312f3598cd255f4c7b77cf7a3755695707049641
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2022-06-27 10:36:52 -0500

    initial commit

---
+++ src/main/java/com/codeup/groovy_spring_blog/GroovySpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.groovy_spring_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovySpringBlogApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(GroovySpringBlogApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/codeup/groovy_spring_blog/GroovySpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.groovy_spring_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovySpringBlogApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
